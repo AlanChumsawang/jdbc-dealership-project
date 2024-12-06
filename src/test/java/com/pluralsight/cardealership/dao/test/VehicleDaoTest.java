1252f98a0faf535817971b52e331857bdd029d2b
AlanChumsawang <alan.chumsawang@outlook.com>
2024-12-06 10:20:28 -0500

    Added more VehicleDaoImpl Test

--- src/test/java/com/pluralsight/cardealership/dao/test/VehicleDaoTest.java
+++ src/test/java/com/pluralsight/cardealership/dao/test/VehicleDaoTest.java
@@ -28,6 +28,54 @@ public class VehicleDaoTest {
         assertEquals(expectedListSize, vehicleList.size());
     }
 
+    @Test
+    void findVehicleByMakeModel() {
+        List<Vehicle> vehicleList = dao.findVehicleByMakeModel("Tesla", "Model Y");
+        int expectedListSize = 1;
+
+        assertEquals(expectedListSize, vehicleList.size());
+    }
+
+    @Test
+    void findVehicleByPrice() {
+        List<Vehicle> vehicleList = dao.findVehicleByPrice(10000, 20000);
+        int expectedListSize = 1;
+
+        assertEquals(expectedListSize, vehicleList.size());
+    }
+
+    @Test
+    void findVehicleByYear() {
+        List<Vehicle> vehicleList = dao.findVehicleByYear(2023);
+        int expectedListSize = 2;
+
+        assertEquals(expectedListSize, vehicleList.size());
+    }
+
+    @Test
+    void findVehicleByColor() {
+        List<Vehicle> vehicleList = dao.findVehicleByColor("Blue");
+        int expectedListSize = 2;
+
+        assertEquals(expectedListSize, vehicleList.size());
+    }
+
+    @Test
+    void findVehicleByMileage() {
+        List<Vehicle> vehicleList = dao.findVehicleByMileage(30000);
+        int expectedListSize = 2;
+
+        assertEquals(expectedListSize, vehicleList.size());
+    }
+
+    @Test
+    void findVehicleByType() {
+        List<Vehicle> vehicleList = dao.findVehicleByType("Sedan");
+        int expectedListSize = 3;
+
+        assertEquals(expectedListSize, vehicleList.size());
+    }
+
 }
 
 
